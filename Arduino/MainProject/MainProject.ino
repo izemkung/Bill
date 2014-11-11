1ebcee0c02aad12c537ecb0a2ef55318ebe5458a
Peradat Permjai <gullwings@msn.com>
2014-11-12 05:10:32 +0700

    12/11/14 05:09
    Save Add function Calc Packet

--- Arduino/MainProject/MainProject.ino
+++ Arduino/MainProject/MainProject.ino
@@ -12,9 +12,32 @@ byte ReadFromRasberry[7];
 byte NumMachine;
 byte Function;
 
-char bufferRxMain[100];
-int indexRxMain = 0;
-int indexReadMain = 0;
+
+//==============Packet Calc===============
+
+#define PRO_START0			_indexStartReader
+#define PRO_START1			_indexStartReader+1
+#define PRO_LENGTH			_indexStartReader+2
+#define PRO_ID                          _indexStartReader+3
+#define PRO_ERROR                       _indexStartReader+4
+#define PRO_VALUE                       _indexStartReader+5
+#define PRO_CHECKSUM		        _indexStartReader+2+rxBuffer[PRO_LENGTH]
+
+#define INC_RING(var, bsize) if (var == ((bsize)-1)) var=0; else var++
+#define CLEAR_BUFFER rxWritePointer = rxReadPointer  = 0
+
+#define RX_BUFFER_SIZE 200
+byte rxBuffer[RX_BUFFER_SIZE];
+byte rxWritePointer = 0;
+byte rxReadPointer = 0;
+
+byte _indexStartReader;
+
+byte _rxState = 0;
+byte _byte_count;
+byte _id;
+byte _checksum;
+//========================================
 
 unsigned long previousMillis = 0;        
 const long interval = 2000; //=================loop time want check status=============================//
@@ -32,32 +55,131 @@ void setup()
 
 void loop() 
 {
-	if (Serial.available())
-	{
-		flagCheckStatus=true;
-	}
-	LoopCheckStatus();
-// 	CalcRxData();
-// 	ReciveBill();
+  Rx_Calc();//Calc packet from raspberry pi
+  LoopCheckStatus();//Check status   
+}
+
+void CalcCommandFromRaspberryPi()
+{
+  byte _id = rxBuffer[PRO_ID];
+  byte _function = rxBuffer[PRO_ID]; 
+  byte _value = rxBuffer[PRO_ID];
+  
+  switch(_id)
+  {
+      case 1:
+      
+                break;
+      case 2:break; 
+      case 3:break; 
+  }
   
 }
-void CalcRxDataMain()
-{
-	while(Serial.available())
-	{
-		bufferRxMain[indexRxMain++] = Serial.read();
-	}
-		
-	if(indexRxMain == indexReadMain)return;
-	int numDatalength = indexRxMain - indexReadMain;
-	//byte data = bufferRx[indexReadMain++];
-	for (int i =0;i<numDatalength;i++)
-	{
-		ReadFromRasberry[i] = bufferRxMain[i];
-	}
-	ClearBufferMain();
-	
+
+//@Overwrite Interrup
+void serialEvent() {
+  while (Serial.available()) 
+  {
+    // get the new byte:
+    rxBuffer[rxWritePointer] = (char)Serial.read(); 
+    INC_RING(rxWritePointer,RX_BUFFER_SIZE);
+    //Serial.println("get");
+  }
 }
+
+void Rx_Calc()
+{
+    if(rxReadPointer != rxWritePointer)
+    {	
+    }else{
+	return;
+    }
+
+    byte _UDR = rxBuffer[rxReadPointer];
+    INC_RING(rxReadPointer,RX_BUFFER_SIZE);
+    
+    switch(state)
+	{
+		case 0://star1
+		if (_UDR == 0xFF)state =1;break;
+		case 1://star2
+		if (_UDR == 0xFF)
+		{
+			state =2;
+			_indexStartReader = rxReadPointer-2;
+		}
+		else
+		{
+			state = 0;
+		}
+		break;
+		
+		case 2://LENGTH
+		_byte_count=_UDR;state =3;if(_UDR > 20)state =0;break;
+
+		case 3://DATA
+		_byte_count--;
+		if (_byte_count == 1) state =4;
+		
+		break;
+		
+		case 4:
+		
+		if(checkSum(PRO_LENGTH,PRO_CHECKSUM)== _UDR)
+		{
+			//Serial.println("check sum ok!");
+		        //CalcParameter();
+                        CalcCommandFromRaspberryPi();
+
+
+                        byte _id = rxBuffer[PRO_ID] -1;
+                        if(_id >= 0 && _id <= LEDNUM)
+                        {
+			  previousMillis[_id] = 0;
+                          LedBuffer[_id][A_R] = rxBuffer[PRO_R];
+                          LedBuffer[_id][A_G] = rxBuffer[PRO_G];
+                          LedBuffer[_id][A_B] = rxBuffer[PRO_B];
+                          LedBuffer[_id][A_MODE] = rxBuffer[PRO_MODE];
+                          LedBuffer[_id][A_TIME] = rxBuffer[PRO_TIME];
+                          LedBuffer[_id][A_TIME2] = rxBuffer[PRO_TIME2];
+                        }else if(_id == 253)//id - 1
+                        {
+                          for(int i = 0;i<=LEDNUM+1;i++)
+                          {
+                            previousMillis[i] = 0;
+                            LedBuffer[i][A_R] = rxBuffer[PRO_R];
+                            LedBuffer[i][A_G] = rxBuffer[PRO_G];
+                            LedBuffer[i][A_B] = rxBuffer[PRO_B];
+                            LedBuffer[i][A_MODE] = rxBuffer[PRO_MODE];
+                            LedBuffer[i][A_TIME] = rxBuffer[PRO_TIME];
+                            LedBuffer[i][A_TIME2] = rxBuffer[PRO_TIME2];
+                          }
+                        }
+
+		}else
+		{
+		}
+		state = 0;
+		break;
+	} 
+    //Debug
+    //Serial.print(_UDR,HEX);
+    //Serial.print(" state = ");
+    //Serial.print(state,HEX);
+    //Serial.print(" bytecount= ");
+    //Serial.print(_byte_count,HEX);
+    //Serial.print("\r\n");
+}
+byte checkSum(byte _start,byte _stop)
+{
+	byte check_Sum = 0;
+	for(;_start != _stop;  _start++)
+	{
+		check_Sum  += rxBuffer[_start];
+	}
+	return ~check_Sum;
+}
+
 
 void SendToRasberry(byte *data,byte lengthR)
 {
@@ -66,9 +188,10 @@ void SendToRasberry(byte *data,byte lengthR)
 		Serial.write((char)(data[i]));
 	}	
 }
+
 void LoopCheckStatus()
 {
-	if(flagCheckStatus==true)return;
+	 if(flagCheckStatus==true)return; //Ennable Disable function
 	 unsigned long currentMillis = millis();	 
 	 if(currentMillis - previousMillis >= interval) {
 		 // save the last time you blinked the LED
@@ -77,10 +200,4 @@ void LoopCheckStatus()
 		 
 	 }
 }
-void ClearBufferMain()
-{
-	for (int i =0;i<100;i++)
-	{
-		bufferRxMain[i] = '\0';
-	}
-}
+
